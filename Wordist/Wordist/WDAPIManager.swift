44e25af1c200854383081ec79babd49d437e1e25
Aamir Anwar <aamir.anwar021193@gmail.com>
2017-09-09 21:43:38 +0530

    Added Alamofire, Service Layer & Word Search Model object

---
+++ Wordist/Wordist/WDAPIManager.swift
@@ -0,0 +1,9 @@
+//
+//  WDAPIManager.swift
+//  Wordist
+//
+//  Created by Aamir  on 09/09/17.
+//  Copyright © 2017 AamirAnwar. All rights reserved.
+//
+
+import Foundation
---
+++ Wordist/Wordist/WDWordSearch.swift
@@ -0,0 +1,9 @@
+//
+//  WDWordSearch.swift
+//  Wordist
+//
+//  Created by Aamir  on 09/09/17.
+//  Copyright © 2017 AamirAnwar. All rights reserved.
+//
+
+import Foundation
