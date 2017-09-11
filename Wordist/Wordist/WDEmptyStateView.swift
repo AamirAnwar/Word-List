d47788e5cb7b31b9f63179a4479160f090c9c403
Aamir Anwar <aamir.anwar021193@gmail.com>
2017-09-12 00:38:58 +0530

    Created empty state view and blackout view

---
+++ Wordist/Wordist/WDEmptyStateView.swift
@@ -0,0 +1,58 @@
+//
+//  WDEmptyStateView.swift
+//  Wordist
+//
+//  Created by Aamir  on 11/09/17.
+//  Copyright © 2017 AamirAnwar. All rights reserved.
+//
+
+import UIKit
+
+class WDEmptyStateView: UIView {
+
+    let iconLabel = UILabel()
+    let messageLabel = UILabel()
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        createEmptyState()
+    }
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        createEmptyState()
+    }
+    
+    func createEmptyState() {
+        self.backgroundColor = UIColor.white
+        iconLabel.textColor = WDMainTheme
+        iconLabel.font = WDIconFontEmptyState
+        iconLabel.translatesAutoresizingMaskIntoConstraints = false
+        iconLabel.textAlignment = .center
+        if let iconString = try? "&#xf4d7;".convertHtmlSymbols() {
+            iconLabel.text = iconString
+        }
+        self.addSubview(self.iconLabel)
+        
+        messageLabel.textColor = WDTextBlack
+        messageLabel.translatesAutoresizingMaskIntoConstraints = false
+        messageLabel.font = WDFontBodyText
+        messageLabel.numberOfLines = 0
+        messageLabel.text = "You dont seem to have any words saved\n Any words you add will show up here!"
+        self.addSubview(messageLabel)
+        
+        NSLayoutConstraint.activate([
+            iconLabel.centerXAnchor.constraint(equalTo: self.centerXAnchor),
+            iconLabel.centerYAnchor.constraint(equalTo: self.centerYAnchor, constant: -kDefaultPadding)
+            ])
+        
+        NSLayoutConstraint.activate([
+            messageLabel.centerXAnchor.constraint(equalTo: iconLabel.centerXAnchor),
+            messageLabel.topAnchor.constraint(equalTo: iconLabel.bottomAnchor, constant: 2*kDefaultPadding),
+            
+            ])
+        
+        
+        
+    }
+}
