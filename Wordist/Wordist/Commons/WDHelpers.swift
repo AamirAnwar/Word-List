67ff6f78a147656993fba494e9e059f26cc8ea3d
Aamir Anwar <aamir.anwar021193@gmail.com>
2017-10-07 21:20:33 +0530

    Added helper functions
    - Computed Property to get tab bar height
    - Computed Property to get tab bar controller
    - Function to check if current view controller is the selected tab controller

--- Wordist/Wordist/Commons/WDHelpers.swift
+++ Wordist/Wordist/Commons/WDHelpers.swift
@@ -10,6 +10,16 @@ import Foundation
 import UIKit
 
 enum WDHelpers {
+    
+    static var tabBarHeight:CGFloat {
+        let tabBarHeight = ((UIApplication.shared.delegate as? AppDelegate)?.window?.rootViewController as? UITabBarController)?.tabBar.frame.height ?? 0.0
+        return tabBarHeight
+    }
+    
+    static var mainTabBarController:UITabBarController? {
+        return ((UIApplication.shared.delegate as? AppDelegate)?.window?.rootViewController as? UITabBarController)
+    }
+    
     static func documentsDirectory() -> URL {
         let paths = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)
         return paths[0]
@@ -19,6 +29,29 @@ enum WDHelpers {
         WDNotificationDropdownView.showWith(message: "There seems to be a problem with the internet :(")
     }
     
+}
+
+extension UIViewController {
+    func isCurrentActiveTabController() -> Bool {
+        guard let appDelegate = (UIApplication.shared.delegate as? AppDelegate), let tabBarController = appDelegate.window?.rootViewController as? UITabBarController else {
+            return false
+        }
+        var activeVC:UIViewController? = nil
+        
+        if let navVC = tabBarController.selectedViewController as? UINavigationController {
+            activeVC = navVC.topViewController
+        }
+        else {
+            activeVC = tabBarController.selectedViewController
+        }
+        
+        if let activeVC = activeVC {
+            if self === activeVC {
+                return true
+            }
+        }
+        return false
+    }
 }
 
 extension String {
