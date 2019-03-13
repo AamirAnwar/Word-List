0d328ae66b556caaef98f3d36b6523c90291053b
Aamir Anwar <aamir.anwar021193@gmail.com>
2019-03-14 01:29:08 +0530

    update danger file

--- Dangerfile.swift
+++ Dangerfile.swift
@@ -12,5 +12,3 @@ warn("Big PR") if git.lines_of_code > 500
 fail("fdescribe left in tests") if `grep -r fdescribe specs/ `.length > 1
 fail("fit left in tests") if `grep -r fit specs/ `.length > 1
 
-let editedFiles = danger.git.modifiedFiles + danger.git.createdFiles
-message("These files have changed: \(editedFiles.joined())")
